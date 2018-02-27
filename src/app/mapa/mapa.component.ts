dd1c7690b26cdaad938f2a1d46ac9b5e1c3048b4
makoki <pablo.makoki@gmail.com>
2018-02-27 19:44:26 +0100

    mapa guay

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild  } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import 'rxjs/add/observable/of';
 import { KonvaComponent } from 'ng2-konva';
-
 
 @Component({
   selector: 'sat-mapa',
@@ -19,25 +19,29 @@ export class MapaComponent implements OnInit {
     this.ImgObj.src = '../../assets/mapa.jpg';
    }
 
-  public configStage = Observable.of({
-    width: 1200,
-    height: 800
-  });
-
-  public imagen = Observable.of({
+  public configStage = new BehaviorSubject({
+    opacity: 0.1,
     x: 50,
     y: 50,
     image: this.ImgObj,
     width: 1200,
-    height: 800,
-    opacity: 0.1
+    height: 800
   });
 
   public handleClick(component) {
-    this.imagen.value.opacity += 0.1;
-    console.log(this.imagen.value);
-    const stageD = this.stage.getStage();
-    stageD.draw();
+    const vImagen = this.configStage.getValue();
+    console.log(vImagen);
+    let opacidadActual = vImagen.opacity;
+    opacidadActual += 0.1;
+
+    this.configStage.next({
+      opacity: opacidadActual,
+      x: 50,
+      y: 50,
+      image: this.ImgObj,
+      width: 1200,
+      height: 800
+    });
   }
 
   ngOnInit() {
